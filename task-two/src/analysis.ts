bdeb1ec33b18c28179da5a8152b1de91b140f243
AlgoDame <chiamakaojiyi@gmail.com>
2021-04-20 15:25:53 +0100

    changes

--- task-two/src/analysis.ts
+++ task-two/src/analysis.ts
@@ -4,8 +4,8 @@
  * @param {string[]} inputPaths An array of csv files to read
  * @param {string} outputPath The path to output the analysis
  */
-import neatCsv from "neat-csv";
-import fs from "fs";
+import neatCsv from 'neat-csv';
+import fs from 'fs';
 function analyseFiles(inputPaths: string[], outputPath: string) {
   //console.log('Complete the implementation in src/analysis.ts');
   for (let input of inputPaths) {
@@ -31,15 +31,15 @@ function analyseFiles(inputPaths: string[], outputPath: string) {
         if (regex.test(email)) {
           validEmails.push(email);
         }
-      })
+      });
       //console.log(validEmails)
       // Getting all domains
       let emailDomains: string[] = [];
       validEmails.forEach((email) => {
-        let index: number = email.indexOf("@") + 1;
+        let index: number = email.indexOf('@') + 1;
         let domainName: string = email.slice(index);
         emailDomains.push(domainName);
-      })
+      });
       //console.log(emailDomains)
       // Getting only the unique domains in an array
       let uniqueDomains: string[] = [...new Set(emailDomains)];
@@ -52,19 +52,19 @@ function analyseFiles(inputPaths: string[], outputPath: string) {
         } else {
           domainCount[domain]++;
         }
-      })
+      });
 
       //console.log(domainCount)
       // Creating an interface Type to format my result
-      interface Format{
-        "valid-domains": string[];
+      interface Format {
+        'valid-domains': string[];
         totalEmailsParsed: number;
         totalValidEmails: number;
-        categories: Record<string, number>
+        categories: Record<string, number>;
       }
       // Creating my result using inteface Type
       let result: Format = {};
-      result["valid-domains"] = uniqueDomains;
+      result['valid-domains'] = uniqueDomains;
       result.totalEmailsParsed = emailsArray.length;
       result.totalValidEmails = validEmails.length;
       result.categories = domainCount;
@@ -73,26 +73,14 @@ function analyseFiles(inputPaths: string[], outputPath: string) {
       //console.log(jsonResult)
 
       // Writing to a file
-      fs.writeFile(outputPath, jsonResult, err => {
+      fs.writeFile(outputPath, jsonResult, (err) => {
         if (err) {
           console.error(err);
           return;
         }
-      })
-
-
-
-
-
-
-
-
-
-    })
+      });
+    });
   }
 }
-
-
-
 
 export default analyseFiles;
